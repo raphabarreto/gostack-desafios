08854db5f217e84126a798f9493dea1ca44763e7
Raphael Barreto <tnt.raphael@gmail.com>
2020-02-21 17:25:14 -0300

    Adicionado Desafio 08

---
+++ 08/README.md
@@ -0,0 +1,35 @@
+<h1 align="center">
+    <img alt="GoStack" src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/bootcamp-header.png" width="200px" />
+</h1>
+
+<h3 align="center">Desafio 8: Utilizando React Hooks</h3>
+
+<p align="center">Nesse projeto foi desenvolvido uma aplicação do zero utilizando <strong>React Native</strong>, migrando o projeto e-commerce do <a href="https://github.com/raphabarreto/gostack-modulos/tree/master/modulo-07">Módulo 07</a> para dispositivos móveis utilizando <strong>React Hooks</strong>.</p>
+
+## 🤖 Resultado
+<img src="rocketshoes/.github/rocketshoes.gif" height="400">
+
+## 🚀 Tecnologias
+
+- ⚡ [JavaScript](https://skylab.rocketseat.com.br/journey/starter)
+- ⚡ [React Native](https://pt-br.reactjs.org/)
+- ⚡ [Redux](https://redux.js.org/)
+- ⚡ [Redux Saga](https://redux-saga.js.org/)
+- ⚡ [Arquitetura Flux](https://facebook.github.io/flux/)
+- ⚡ [React Hooks](https://pt-br.reactjs.org/docs/hooks-intro.html)
+
+## ✋🏻 Pré-requisitos
+
+- ⚡ [Yarn](https://yarnpkg.com/pt-BR/docs/install)
+- ⚡ [Ambiente de configuração - Android ou iOS](https://docs.rocketseat.dev/ambiente-react-native/introducao)
+
+## 🔥 Instalação e execução
+
+1. Faça um clone desse repositório;
+2. Entre na pasta `cd rocketshoes`;
+3. Rode `yarn` para instalar todas as dependências;
+4. Rode `npx react-native run-android` para instalar a aplicação seja no seu smartphone ou emulador;
+---
+
+Feito com 💖 por [Raphael Barreto](https://raphabarreto.com.br/)
+graças a [RocketSeat](https://rocketseat.com.br/)🚀
---
+++ 08/rocketshoes/src/pages/Cart/index.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import * as CartActions from '../../store/modules/cart/actions';
+
+import { formatPrice } from '../../util/format';
+import colors from '../../styles/colors';
+
+import {
+  Container,
+  Products,
+  Product,
+  ProductInfo,
+  ProductImage,
+  ProductDetails,
+  ProductTitle,
+  ProductPrice,
+  ProductDelete,
+  ProductControls,
+  ProductControlButton,
+  ProductAmount,
+  ProductSubtotal,
+  TotalContainer,
+  TotalText,
+  TotalAmount,
+  Order,
+  OrderText,
+  EmptyContainer,
+  EmptyText,
+} from './styles';
+
+export default function Cart() {
+  const cart = useSelector(state =>
+    state.cart.map(product => ({
+      ...product,
+      subtotal: formatPrice(product.price * product.amount),
+      priceFormatted: formatPrice(product.price),
+    }))
+  );
+
+  const total = useSelector(state =>
+    formatPrice(
+      state.cart.reduce((sumTotal, product) => {
+        return sumTotal + product.price * product.amount;
+      }, 0)
+    )
+  );
+
+  const dispatch = useDispatch();
+
+  function decrement(product) {
+    dispatch(CartActions.updateAmountRequest(product.id, product.amount - 1));
+  }
+
+  function increment(product) {
+    dispatch(CartActions.updateAmountRequest(product.id, product.amount + 1));
+  }
+
+  return (
+    <Container>
+      {cart.length ? (
+        <>
+          <Products>
+            {cart.map(product => (
+              <Product key={product.id}>
+                <ProductInfo>
+                  <ProductImage source={{ uri: product.image }} />
+                  <ProductDetails>
+                    <ProductTitle>{product.title}</ProductTitle>
+                    <ProductPrice>{product.priceFormatted}</ProductPrice>
+                  </ProductDetails>
+                  <ProductDelete
+                    onPress={() =>
+                      dispatch(CartActions.removeFromCart(product.id))
+                    }
+                  >
+                    <Icon
+                      name="delete-forever"
+                      size={24}
+                      color={colors.primary}
+                    />
+                  </ProductDelete>
+                </ProductInfo>
+                <ProductControls>
+                  <ProductControlButton onPress={() => decrement(product)}>
+                    <Icon
+                      name="remove-circle-outline"
+                      size={20}
+                      color={colors.primary}
+                    />
+                  </ProductControlButton>
+                  <ProductAmount value={String(product.amount)} />
+                  <ProductControlButton onPress={() => increment(product)}>
+                    <Icon
+                      name="add-circle-outline"
+                      size={20}
+                      color={colors.primary}
+                    />
+                  </ProductControlButton>
+                  <ProductSubtotal>{product.subtotal}</ProductSubtotal>
+                </ProductControls>
+              </Product>
+            ))}
+          </Products>
+          <TotalContainer>
+            <TotalText>TOTAL</TotalText>
+            <TotalAmount>{total}</TotalAmount>
+            <Order>
+              <OrderText>FINALIZAR PEDIDO</OrderText>
+            </Order>
+          </TotalContainer>
+        </>
+      ) : (
+        <EmptyContainer>
+          <Icon name="remove-shopping-cart" size={64} color="#eee" />
+          <EmptyText>Seu carrinho está vazio.</EmptyText>
+        </EmptyContainer>
+      )}
+    </Container>
+  );
+}
