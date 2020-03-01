ac9c564d310e58d516c82a7bf1692e179f775950
Raphael Barreto <tnt.raphael@gmail.com>
2020-03-01 12:30:35 -0300

    Requisições autenticadas

--- 09/src/pages/Delivery/index.js
+++ 09/src/pages/Delivery/index.js
@@ -1,7 +1,10 @@
 import React from "react";
+import api from "~/services/api";
 
 // import { Container } from './styles';
 
 export default function Delivery() {
+  api.get("problems");
+
   return <h1>Delivery</h1>;
 }
--- 09/src/store/modules/auth/sagas.js
+++ 09/src/store/modules/auth/sagas.js
@@ -22,6 +22,8 @@ export function* signIn({ payload }) {
       return;
     }
 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
     yield put(signInSuccess(token, user));
 
     history.push("/delivery");
@@ -31,4 +33,18 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest("@auth/SIGN_IN_REQUEST", signIn)]);
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export default all([
+  takeLatest("persist/REHYDRATE", setToken),
+  takeLatest("@auth/SIGN_IN_REQUEST", signIn),
+  takeLatest("@auth/SIGN_FAILURE", signFailure)
+]);
