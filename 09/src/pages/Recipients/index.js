10ce71f81173078f9e2d4a6283f786b5251a3292
Raphael Barreto <tnt.raphael@gmail.com>
2020-03-06 15:29:59 -0300

    Adicionado página de destinatários

--- 09/src/pages/Recipients/index.js
+++ 09/src/pages/Recipients/index.js
@@ -1,7 +1,143 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-// import { Container } from './styles';
+import { Input } from '@rocketseat/unform';
+
+import { FiPlus } from 'react-icons/fi';
+import { FaEllipsisH } from 'react-icons/fa';
+
+import { MdSearch, MdEdit, MdDeleteForever } from 'react-icons/md';
+import { toast } from 'react-toastify';
+
+import api from '~/services/api';
+
+import {
+  Container,
+  RecipientsTable,
+  Header,
+  Search,
+  ActionButton,
+  ActionList,
+} from './styles';
 
 export default function Recipients() {
-  return <h1>Recipients</h1>;
+  const [recipients, setRecipients] = useState([]);
+  const [q, setQ] = useState('');
+
+  useEffect(() => {
+    async function loadRecipients() {
+      const response = await api.get('recipients', {
+        params: { q },
+      });
+
+      const { data } = response;
+
+      setRecipients(data);
+    }
+    loadRecipients();
+  }, [q]);
+
+  function handleToggleActions(id) {
+    const updateRecipients = recipients.map(d => {
+      if (d.id === id) {
+        d.visible = !d.visible;
+      }
+      return d;
+    });
+
+    setRecipients(updateRecipients);
+  }
+
+  async function handleRemove(id) {
+    const removeAlert = window.confirm(
+      'Tem certeza que quer excluir o entregador?'
+    );
+    if (!removeAlert) {
+      return;
+    }
+    try {
+      await api.delete(`/recipients/${id}`);
+
+      const newRecipients = recipients.filter(d => d.id !== id);
+
+      setRecipients(newRecipients);
+    } catch (err) {
+      toast.error('Falha ao excluir destinatário!');
+    } finally {
+      toast.success('Destinatário excluído com sucesso!');
+    }
+  }
+
+  return (
+    <Container>
+      <p>Gerenciando destinatários</p>
+      <Header>
+        <Search>
+          <MdSearch size={20} color="#999999" />
+          <Input
+            name="destinátarios"
+            placeholder="Buscar por destinátarios"
+            onChange={e => setQ(e.target.value)}
+          />
+        </Search>
+        <button type="button">
+          <FiPlus size={24} className="react-icons" /> CADASTRAR
+        </button>
+      </Header>
+
+      <RecipientsTable>
+        <thead>
+          <th>ID</th>
+          <th>Nome</th>
+          <th>Endereço</th>
+          <th>Ações</th>
+        </thead>
+        <tbody>
+          {recipients.map(recipient => (
+            <tr key={recipient.id}>
+              <td>#{recipient.id}</td>
+              <td>{recipient.name}</td>
+              <td>
+                {recipient.street}, {recipient.number}, {recipient.city} -{' '}
+                {recipient.state}
+              </td>
+              <td>
+                <ActionButton>
+                  <button
+                    type="button"
+                    onClick={() => handleToggleActions(recipient.id)}
+                  >
+                    <FaEllipsisH size={24} />
+                  </button>
+
+                  <ActionList visible={recipient.visible}>
+                    <button type="button">
+                      <MdEdit
+                        size={24}
+                        color="#4D85EE"
+                        style={{ marginRight: 5 }}
+                      />
+                      Editar
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => {
+                        handleRemove(recipient.id);
+                      }}
+                    >
+                      <MdDeleteForever
+                        size={24}
+                        color="#DE3B3B"
+                        style={{ marginRight: 5 }}
+                      />
+                      Excluir
+                    </button>
+                  </ActionList>
+                </ActionButton>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </RecipientsTable>
+    </Container>
+  );
 }
---
+++ 09/src/pages/Recipients/styles.js
@@ -0,0 +1,146 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 90%;
+  margin: 50px auto;
+
+  p {
+    display: flex;
+    align-self: flex;
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 30px;
+  }
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  input {
+    width: 237px;
+    height: 36px;
+    border: 2px solid #dddddd;
+    border-radius: 5px;
+  }
+
+  input::placeholder {
+    display: flex;
+    padding-left: 5px;
+  }
+
+  input {
+    box-sizing: border-box;
+    padding: 15px;
+  }
+
+  button {
+    width: 150px;
+    height: 40px;
+    border: 1px solid;
+    border-radius: 5px;
+    background: #7d40e7;
+    color: #fff;
+    font-weight: bold;
+  }
+`;
+
+export const Search = styled.div`
+  background: #fff;
+  border: 1px solid #dddddd;
+  box-sizing: border-box;
+  border-radius: 4px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 3px 8px;
+
+  input {
+    border: 0;
+    font-size: 14px;
+    color: #999999;
+  }
+`;
+
+export const RecipientsTable = styled.table`
+  border-spacing: 0 10px;
+  width: 100%;
+
+  thead th {
+    font-size: 18px;
+    font-weight: bold;
+    padding: 20px 15px;
+    text-align: left;
+    color: #444444;
+  }
+
+  tbody tr {
+    background-color: #ffffff;
+  }
+
+  tbody td {
+    font-size: 16px;
+    padding: 20px;
+    padding-left: 15px;
+    text-align: left;
+    color: #666666;
+  }
+  td {
+    width: 100px;
+  }
+
+  td > img {
+    width: 20%;
+    border-radius: 50%;
+  }
+
+  td:first-child {
+    border-top-left-radius: 0.9em;
+    border-bottom-left-radius: 0.9em;
+  }
+
+  td:last-child {
+    border-top-right-radius: 0.9em;
+    border-bottom-right-radius: 0.9em;
+    text-align: right;
+    color: #c6c6c6;
+  }
+
+  thead th:last-child {
+    text-align: right;
+  }
+`;
+
+export const ActionButton = styled.div`
+  button {
+    border: none;
+    background: none;
+    color: #999999;
+  }
+`;
+
+export const ActionList = styled.div`
+  display: ${props => (props.visible ? 'flex' : 'none')};
+  flex-direction: column;
+  position: absolute;
+  background: #fff;
+  width: 150px;
+  height: 90px;
+  right: 50px;
+  border-radius: 4px;
+  padding: 15px 5px;
+
+  a,
+  button {
+    display: flex;
+    align-items: center;
+    background: #fff;
+    border-bottom: 1px solid #ddd;
+    color: #999999;
+    margin-top: 5px;
+
+    &:last-child {
+      border-bottom: 0;
+    }
+  }
+`;
