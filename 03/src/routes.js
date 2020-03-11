cfb34ff592ac82fd6c46afd02b4d2684970da8f6
Raphael Barreto <tnt.raphael@gmail.com>
2020-03-11 20:29:22 -0300

    Alterado rotas não-autenticadas

--- 03/src/routes.js
+++ 03/src/routes.js
@@ -19,30 +19,14 @@ import authMiddleware from './app/middlewares/auth';
 const routes = new Router();
 const upload = multer(multerConfig);
 
+// Rotas não autenticadas
+
 routes.post('/sessions', SessionController.store);
 
 routes.post('/users', UserController.store);
 routes.put('/users', UserController.update);
 
-routes.use(authMiddleware);
-
-routes.get('/recipients', RecipientController.index);
-routes.get('/recipients/:recipientId', RecipientController.show);
-routes.post('/recipients', RecipientController.store);
-routes.put('/recipients/:recipientId', RecipientController.update);
-routes.delete('/recipients/:recipientId', RecipientController.delete);
-
-routes.get('/deliverymen', DeliverymanController.index);
 routes.get('/deliverymen/:deliverymanId', DeliverymanController.show);
-routes.post('/deliverymen/', DeliverymanController.store);
-routes.put('/deliverymen/:deliverymanId', DeliverymanController.update);
-routes.delete('/deliverymen/:deliverymanId', DeliverymanController.delete);
-
-routes.get('/deliveries', DeliveryController.index);
-routes.get('/deliveries/:deliveryId', DeliveryController.show);
-routes.post('/deliveries', DeliveryController.store);
-routes.put('/deliveries/:deliveryId', DeliveryController.update);
-routes.delete('/deliveries/:deliveryId', DeliveryController.delete);
 
 routes.get(
   '/deliveryman/:deliverymanId/deliveries',
@@ -74,11 +58,29 @@ routes.get('/problems', DeliveryProblemsController.index);
 routes.get('/problems/:deliveryId', DeliveryProblemsController.show);
 routes.delete('/problems/:problemId', DeliveryProblemsController.delete);
 
-routes.post(
-  '/files',
-  authMiddleware,
-  upload.single('file'),
-  FileController.store
-);
+routes.post('/files', upload.single('file'), FileController.store);
+
+// Autenticação
+routes.use(authMiddleware);
+
+// Rotas autenticadas
+
+routes.get('/recipients', RecipientController.index);
+routes.get('/recipients/:recipientId', RecipientController.show);
+routes.post('/recipients', RecipientController.store);
+routes.put('/recipients/:recipientId', RecipientController.update);
+routes.delete('/recipients/:recipientId', RecipientController.delete);
+
+routes.get('/deliverymen', DeliverymanController.index);
+
+routes.post('/deliverymen/', DeliverymanController.store);
+routes.put('/deliverymen/:deliverymanId', DeliverymanController.update);
+routes.delete('/deliverymen/:deliverymanId', DeliverymanController.delete);
+
+routes.get('/deliveries', DeliveryController.index);
+routes.get('/deliveries/:deliveryId', DeliveryController.show);
+routes.post('/deliveries', DeliveryController.store);
+routes.put('/deliveries/:deliveryId', DeliveryController.update);
+routes.delete('/deliveries/:deliveryId', DeliveryController.delete);
 
 export default routes;
