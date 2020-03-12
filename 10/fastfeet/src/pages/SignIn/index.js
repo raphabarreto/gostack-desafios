dea2eba8430cda5bca42af8d3c24de88ad30aee8
Raphael Barreto <tnt.raphael@gmail.com>
2020-03-11 21:10:54 -0300

    Adicionado página SignIn

--- 10/fastfeet/src/pages/SignIn/index.js
+++ 10/fastfeet/src/pages/SignIn/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 import logo from '~/assets/logo.png';
 
 import { Container, Form, FormInput, SubmitButton } from './styles';
+import { signInRequest } from '~/store/modules/auth/actions';
 
-export default function SignIn() {
-  function handleSumbmit() { }
+export default function SignIn({ navigation }) {
+  const dispatch = useDispatch();
+
+  const [id, setId] = useState('');
+
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSumbmit() {
+    dispatch(signInRequest(id));
+  }
 
   return (
     <Container>
@@ -17,9 +27,14 @@ export default function SignIn() {
           autoCorrect={false}
           autoCapitalize="none"
           placeholder="Informe seu ID de cadastro"
+          keyboardType="numeric"
+          value={id}
+          onChangeText={setId}
         />
 
-        <SubmitButton onPress={handleSumbmit}>Entrar no sistema</SubmitButton>
+        <SubmitButton loading={loading} onPress={handleSumbmit}>
+          Entrar no sistema
+        </SubmitButton>
       </Form>
     </Container>
   );
---
+++ 10/fastfeet/src/store/modules/auth/sagas.js
@@ -0,0 +1,25 @@
+import { Alert } from 'react-native';
+
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+
+import api from '~/services/api';
+
+import { signInSuccess, signFailure } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { id } = payload;
+
+    const { data } = yield call(api.get, `deliverymen/${id}`);
+
+    yield put(signInSuccess(data));
+  } catch (err) {
+    Alert.alert(
+      'Falha na autenticação',
+      'Houve um erro no login, verifique seus dados'
+    );
+    yield put(signFailure());
+  }
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
