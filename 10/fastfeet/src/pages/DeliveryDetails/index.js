d9d931c31267ddf3f06f2cd4cfaa22d136fdb8be
Raphael Barreto <tnt.raphael@gmail.com>
2020-03-13 17:16:19 -0300

    Adicionado página de detalhes da encomenda

--- 10/fastfeet/src/pages/DeliveryDetails/index.js
+++ 10/fastfeet/src/pages/DeliveryDetails/index.js
@@ -1,15 +1,111 @@
 import React from 'react';
-import { TouchableOpacity, View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import { format } from 'date-fns';
 
-export default function DeliveryDetails() {
-  return <View />;
+import {
+  Container,
+  Background,
+  DeliveryContainer,
+  DeliveryInfo,
+  Header,
+  HeaderText,
+  Title,
+  Info,
+  Status,
+  DateContainer,
+  ActionsContainer,
+  Action,
+  ActionText,
+} from './styles';
+
+export default function DeliveryDetails({ navigation }) {
+  const data = navigation.getParam('data');
+
+  if (data.end_date) {
+    data.status = 'Entregue';
+  } else if (data.canceled_at) {
+    data.status = 'Cancelada';
+  } else if (data.start_date) {
+    data.status = 'Retirada';
+  } else {
+    data.status = 'Pendente';
+  }
+
+  return (
+    <Container>
+      <Background />
+      <DeliveryContainer>
+        <DeliveryInfo>
+          <Header>
+            <Icon name="local-shipping" color="#7D40E7" size={30} />
+            <HeaderText>Informações da entrega</HeaderText>
+          </Header>
+          <Title>Destinatário</Title>
+          <Info>{data.recipient.name}</Info>
+          <Title>Endereço de entrega</Title>
+          <Info>
+            {data.recipient.street}, {data.recipient.number},
+            {data.recipient.complement && ` - ${data.recipient.complement} `}{' '}
+            {data.recipient.city} - {data.recipient.state}
+            {data.recipient.zipCode}
+          </Info>
+          <Title>Produto</Title>
+          <Info>{data.product}</Info>
+        </DeliveryInfo>
+
+        <DeliveryInfo>
+          <Header>
+            <Icon name="event" color="#7D40E7" size={30} />
+            <HeaderText>Situação da entrega</HeaderText>
+          </Header>
+          <Title>Status</Title>
+          <Status>{data.status}</Status>
+          <DateContainer>
+            <Title>Data de retirada</Title>
+            <Title>Data de entrega</Title>
+          </DateContainer>
+          <DateContainer>
+            <Info>
+              {data.start_date
+                ? format(new Date(data.start_date), 'dd/MM/yyyy')
+                : '--/--/--'}
+            </Info>
+            <Info>
+              {data.end_date
+                ? format(new Date(data.end_date), 'dd/MM/yyyy')
+                : '--/--/--'}
+            </Info>
+          </DateContainer>
+        </DeliveryInfo>
+      </DeliveryContainer>
+      <ActionsContainer>
+        <Action>
+          <Icon name="highlight-off" size={24} color="#E74040" />
+          <ActionText>Informar Problema</ActionText>
+        </Action>
+        <Action>
+          <Icon name="info-outline" size={24} color="#E7BA40" />
+          <ActionText>Visualizar Problemas</ActionText>
+        </Action>
+        {!data.end_date ? (
+          <Action>
+            <Icon name="check" size={24} color="#82BF18" />
+            <ActionText>Fazer a retirada</ActionText>
+          </Action>
+        ) : (
+            <Action>
+              <Icon name="alarm-on" size={24} color="#7D40E7" />
+              <ActionText>Confirmar Entrega</ActionText>
+            </Action>
+          )}
+      </ActionsContainer>
+    </Container>
+  );
 }
 
 DeliveryDetails.navigationOptions = ({ navigation }) => ({
-  headerShown: 'screen',
   title: 'Detalhes da encomenda',
 
   headerLeft: () => (
@@ -18,7 +114,7 @@ DeliveryDetails.navigationOptions = ({ navigation }) => ({
         navigation.navigate('Dashboard');
       }}
     >
-      <Icon name="chevron-left" size={20} color="#fff" />
+      <Icon name="chevron-left" size={30} color="#fff" />
     </TouchableOpacity>
   ),
 });
--- 10/fastfeet/src/pages/DeliveryDetails/styles.js
+++ 10/fastfeet/src/pages/DeliveryDetails/styles.js
@@ -1,3 +1,85 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  background: #fff;
+`;
+
+export const Background = styled.View`
+  background-color: #7d40e7;
+  height: 155px;
+`;
+
+export const DeliveryContainer = styled.View`
+  margin-top: -55px;
+`;
+
+export const DeliveryInfo = styled.View`
+  background-color: #fff;
+  border-radius: 2px;
+  padding: 8px;
+  margin: 0px 30px 15px;
+  border: 0.5px;
+  border-color: rgba(158, 150, 150, 0.5);
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 5px;
+`;
+
+export const HeaderText = styled.Text`
+  color: #7d40e7;
+  font-weight: bold;
+  margin-left: 10px;
+`;
+
+export const Title = styled.Text`
+  color: #999999;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 14px;
+  margin-bottom: 5px;
+`;
+
+export const Info = styled.Text`
+  color: #666666;
+  font-size: 14px;
+  margin-bottom: 20px;
+`;
+
+export const Status = styled.Text`
+  color: #666666;
+  font-size: 14px;
+  margin-bottom: 20px;
+`;
+
+export const DateContainer = styled.View`
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+export const ActionsContainer = styled.View`
+  margin: 0px 30px;
+  background-color: #f8f9fd;
+  border-radius: 4px;
+  box-shadow: 0 0 3px #0000001a;
+  flex-direction: row;
+`;
+
+export const Action = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+  border: 1px solid #0000001a;
+`;
+
+export const ActionText = styled.Text`
+  margin-top: 2px;
+  color: #999999;
+  font-size: 12px;
+  text-align: center;
+`;
