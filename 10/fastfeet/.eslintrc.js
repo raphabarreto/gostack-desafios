a37d791f15f8bbc0ff8e7d31c65f54a0708d6e0d
Raphael Barreto <tnt.raphael@gmail.com>
2020-03-16 18:46:10 -0300

    Adicionado regra para não exibir display name

--- 10/fastfeet/.eslintrc.js
+++ 10/fastfeet/.eslintrc.js
@@ -33,6 +33,7 @@ module.exports = {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
     "react/jsx-props-no-spreading": "off",
+    "react/display-name": "off"
   },
   settings: {
     "import/resolver": {
